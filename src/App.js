aea6de566a17166edbd3df5baee3413e9f36819a
bigyandev <bigyanghimire2020@gmail.com>
2023-10-16 22:41:11 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useQuery,useMutation,useQueryClient } from '@tanstack/react-query';
+
+
+const posts = [
+  {id:1, title:"post 1"},
+  {id:"2", title: "post 2"}
+]
+const wait = (duration) => {
+  return new Promise(resolve => setTimeout(resolve, duration))
+}
 function App() {
+  const queryClient = useQueryClient()
+  const postQuery = useQuery({
+    queryKey: ["post"],
+    queryFn: () => wait(1000).then(() => [...posts]) 
+  })
+
+  const newPostMutation = useMutation({
+    mutationFn: title => wait(1000).then(() => posts.push({id:crypto.randomUUID(),title})),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["post"])
+  }
+  })
+
+  if(postQuery.isLoading) return <h1>...loading</h1>
+  if(postQuery.error) return <h1>{postQuery.error}</h1>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {postQuery.data.map((post) => {
+        return  <div key={post.id}>{post.title}</div>
+    })}
+    <button onClick={() => newPostMutation.mutate("new post")}>click</button>
+    </>
+  )
+  
 }
 
 export default App;
